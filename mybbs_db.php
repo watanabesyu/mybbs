b1cd3a9765b2207de67accd090bb371c74b93c69
watanabesyu <watanabesyu@ppfpart.co.jp>
2020-04-01 21:37:36 +0900

    all

--- mybbs_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-//DBに接続
-    //本番
-    //$pdo = new PDO ('mysql:dbname=ppftech_db3;host=;carset=utf8','','');
-    //ローカル
-    //DB:mysql DBname:mybbs host:localhost user:mybbs pass:mybbs
-    $pdo = new PDO ('mysql:dbname=mybbs;host=localhost;carset=utf8mb4_general_ci','mybbs','mybbs');
-?>
