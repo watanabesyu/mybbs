d009b800a6e733122959d82dd4a0ae103cd37008
watanabesyu <62925249+watanabesyu@users.noreply.github.com>
2020-04-01 21:04:01 +0900

    Delete mybbs_db.php

--- mybbs_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //DBに接続
-        //本番用
-        //$pdo = new PDO ('mysql:dbname=ppftech_db3;host=mysql1.php.xdomain.ne.jp;carset=utf8','ppftech_user3','user1234');
-        //ローカル用
-        $pdo = new PDO ('mysql:dbname=mybbs_db;host=localhost;carset=utf8','watanabe','wata0410');
-?>
